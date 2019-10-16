0ad90717c97cfdbaff38c16d704bb61b2788afc8
lucky <834104639@qq.com>
2019-10-16 18:29:28 +0800

    countdown

--- js/Jump.js
+++ js/Jump.js
@@ -1,12 +1,13 @@
-function refer(){
-	if(t==0){
-		location="https://dickenstone.github.io"; // #设定跳转的链接地址
+function Countdown(){、
+	if(window.confirm("sometext") == true){
+		clearInterval();
 	}
-	document.getElementById('count').innerHTML=""+t+"秒后跳转到主页"; // 显示倒计时
-	t--; // 计数器递减
-}
-function jump(){
-	window.alert("404 shold show");
-	var t=5;// 设定跳转的时间
-	setInterval("refer()",1000); // 启动1秒定时
+	var t = 5;
+	return funtion(){
+		if(t == 0){
+			location = "https://dickenstone.github.io";
+		}
+		return t -= 1;
+	}
+	
 }
